67f3fcb5d13b5a860e48694a561a0840812fb3a4
wswind <18399096+wswind@users.noreply.github.com>
2019-11-23 11:31:54 +0800

    add httpapi call with no auth

---
+++ src/httpapicall/api1/Services/ICallService.cs
@@ -0,0 +1,11 @@
+﻿using WebApiClient;
+using WebApiClient.Attributes;
+
+namespace api1.Services
+{
+    public interface ICallService : IHttpApi
+    {
+        [HttpGet("Double")]
+        ITask<int> GetDouble(int i);
+    }
+}
---
+++ src/httpapicall/api1/Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebApiClient;
+using api1.Services;
+
+namespace api1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            string doubleUrl = Configuration.GetValue<string>("DoubleUrl");
+            HttpApi.Register<ICallService>().ConfigureHttpApiConfig(c =>
+            {
+                c.HttpHost = new Uri(doubleUrl);
+                //c.FormatOptions.DateTimeFormat = DateTimeFormats.ISO8601_WithMillisecond;
+            }); ;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
